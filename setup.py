7add3e2072c2455a08b25057d0122add925dc0a3
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-19 13:22:43 +0530

    Project structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The aim of the project is to predict occurance of thyroid in patients given some info regarding the patient',
+    author='Shivam Shinde',
+    license='MIT',
+)
